77f0d85064ef8e864c1bfd3e2c01690f132cba1d
papasmf1 <papasmf1@gmail.com>
2022-09-27 14:46:16 +0900

    클래스에서 이름 숨김 연습

--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -1,9 +1,12 @@
 class Person:
     pass
+
 class Bird:
     pass
+
 class Student(Person):
     pass
+
 p, s = Person(), Student()
 
 print("p is instance of Person: ", isinstance(p, Person))
@@ -11,3 +14,4 @@ print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))
 print("p is instance of Bird: ", isinstance(p, Bird))
 print("int is instance of Object: ", isinstance(int, object))
+
