4388024a5b860e8deea6d5e6920a613934f22f7e
papasmf1 <papasmf1@gmail.com>
2022-09-27 14:59:46 +0900

    클래스 상속 연습

--- 상속01.py
+++ 상속01.py
@@ -1,12 +1,15 @@
+#부모 클래스를 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, 
+            self.phoneNumber))
 
+#자식 클래스 정의 
 class Student(Person):
+    #상속받아서 마음에 안들면 재정의(덮어쓰기)
     def __init__(self, name, phoneNumber, subject, studentID):
         self.name = name
         self.phoneNumber = phoneNumber
@@ -15,8 +18,8 @@ class Student(Person):
 
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "빅데이터", "191122")
+p.printInfo()
+s.printInfo()
 
 
