a92a88aac0bfa9ad1f20bb9d13a6818d520b1e1f
papasmf1 <papasmf1@gmail.com>
2022-09-28 11:35:01 +0900

    문자열 처리 메서드 연습2

--- demoStr.py
+++ demoStr.py
@@ -9,3 +9,29 @@ print( strA.capitalize() )
 print( strA.upper() )
 print( strA.count("p") )
 print( strA.count("p", 7) )
+
+print("---반복 문구---")
+names = ["홍길동","전우치","이순신"]
+for name in names:
+    print("안녕하세요 {0}님 멋진 가을입니다.".format(name))
+    print("=" * 40)
+
+print( "demo.ppt".startswith("demo") )
+print( "demo.ppt".endswith("ppt") )
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
+
+print("---문자열 전처리---")
+u = "<<< spam and ham >>>"
+result = u.strip("<> ")
+print(u)
+print(result)
+result = result.replace("spam", "spam egg")
+print(result)
+print("---리스트---")
+lst = result.split()
+print(lst)
+print("---다시 합치기---")
+print( " ".join(lst) )
+
