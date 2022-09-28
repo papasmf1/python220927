63ee73464e859339c9996c86b86e72badc3ed6de
papasmf1 <papasmf1@gmail.com>
2022-09-28 16:19:32 +0900

    DB연습2

--- db1.py
+++ db1.py
@@ -1,6 +1,5 @@
 # db1.py
 import sqlite3
-
 #연결객체(일단은 메모리에서 작업)
 con = sqlite3.connect(":memory:")
 #구문을 실행하는 커서 
@@ -9,6 +8,10 @@ cur = con.cursor()
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-111');")
+#입력 파라메터 처리
+name = "gildong"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
 #검색
 cur.execute("select * from PhoneBook;")
 for row in cur:
