1032c9d15341f68181339a10457e2dd6be83f2ca
papasmf1 <papasmf1@gmail.com>
2022-09-29 13:18:48 +0900

    클리앙 파일로 저장

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -7,6 +7,7 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&category=0&po=' + str(n)
@@ -20,17 +21,18 @@ for n in range(0,10):
         soup = BeautifulSoup(page, 'html.parser')
         #속성들(attrs)
         list = soup.find_all('span', attrs={'data-role':'list-title-text'})
-
         # <span class="subject_fixed" data-role="list-title-text" title="맥북프로 2020 13인치 터치바 판매합니다">
         # 맥북프로 2020 13인치 터치바 판매합니다
         # </span>
-
         for item in list:
                 try:
                         title = item.text.strip()
-                        print(title)
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
+                        #print(title)
+                        if (re.search('맥북', title)):
+                                print(title.strip())
+                                f.write(title.strip() + "\n")
                 except:
                         pass
+
+f.close()
         
