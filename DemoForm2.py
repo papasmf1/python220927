3853939fc5ea9499aad49e7896b9ffe3d89e28c4
papasmf1 <papasmf1@gmail.com>
2022-09-29 16:19:06 +0900

    폼에 크롤링 코드 연결하기

--- DemoForm2.py
+++ DemoForm2.py
@@ -3,6 +3,12 @@
 import sys 
 from PyQt5.QtWidgets import * 
 from PyQt5 import uic 
+#웹서버에 요청
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
+
+
 #화면로딩(DemoForm2.ui)
 form_class = uic.loadUiType("DemoForm2.ui")[0]
 #클래스 정의(부모 클래스변경)
@@ -12,7 +18,20 @@ class DemoForm(QMainWindow, form_class):
         self.setupUi(self)
     #시그널을 처리하는 슬롯메서드
     def firstClick(self):
-        self.label.setText("첫번째 버튼을 클릭")        
+        f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+        for i in range(1,6):
+            url = \
+            "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+            print(url)
+            data = urllib.request.urlopen(url)
+            soup = BeautifulSoup(data, "html.parser")
+            cartoons = soup.find_all("td", class_="title")
+            for item in cartoons:
+                title = item.find("a").text.strip()
+                print(title)
+                f.write(title + "\n")
+        f.close() 
+        self.label.setText("네이버 웹툰을 크롤링~~")        
     def secondClick(self):
         self.label.setText("두번째 버튼 클릭~~")  
     def thirdClick(self):
