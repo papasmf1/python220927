839fe1d4fc28bc7f1ae167b8307e8dea3d8bc943
papasmf1 <papasmf1@gmail.com>
2022-09-29 14:50:27 +0900

    폼 연습2

--- DemoForm.py
+++ DemoForm.py
@@ -3,17 +3,14 @@
 import sys 
 from PyQt5.QtWidgets import * 
 from PyQt5 import uic 
-
 #화면로딩
 form_class = uic.loadUiType("Demoform.ui")[0]
-
 #클래스 정의
 class DemoForm(QDialog, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.label.setText("첫번째 화면")
-
 #직접 모듈을 실행한 경우(진입점, main()함수)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
