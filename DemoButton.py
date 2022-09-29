57bf964c326baf3f2053c09192268702b4307270
papasmf1 <papasmf1@gmail.com>
2022-09-29 16:45:05 +0900

    코드로 화면을 만들기

---
+++ DemoButton.py
@@ -0,0 +1,22 @@
+# DemoButton.py
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class DemoWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        btn1 = QPushButton("닫기", self)
+        #좌측상단(x,y)
+        btn1.move(20, 20)
+        #시그널 + 슬롯 메서드 
+        btn1.clicked.connect(QCoreApplication.instance().quit)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demoWindow = DemoWindow()
+    demoWindow.show()
+    app.exec_() 
